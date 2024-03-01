239a14ef69ce5f7c11840a9199bc8ca4693b1a95
edgarp <edgarpc0495@gmail.com>
2024-03-01 12:14:45 -0500

    se agrego el tag de google search

---
+++ src/pages/api/cloudinary.ts
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dtkzfmsbs', 
+  api_key: '221525791468519', 
+  api_secret: 'kVZZLReW--JLiKohla8AvBUjvVc' 
+});
